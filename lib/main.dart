32e3002700a7b069095b60b949d11f76bb68e660
Peter Aleksander Bizjak <peter.aleksander.bizjak@ipmgroup.si>
2020-10-09 11:57:46 +0200

    Introduce Mapbox and location plugins, update minimum SDK version for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:mappy/utils/config.helper.dart';
+import 'package:mappy/utils/location.helper.dart';
 
 void main() {
   runApp(App());
@@ -11,7 +15,13 @@ const Color COLOR_ACCENT = const Color(0xFF006992);
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: COLOR_SECONDARY,
+      ),
+    );
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: COLOR_PRIMARY,
         accentColor: COLOR_ACCENT,
@@ -29,6 +39,44 @@ class App extends StatelessWidget {
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: FutureBuilder(
+        future: loadConfigFile(),
+        builder: (
+          BuildContext cntx,
+          AsyncSnapshot<Map<String, dynamic>> snapshot,
+        ) {
+          if (snapshot.hasData) {
+            return MapboxMap(
+              accessToken: snapshot.data['mapbox_api_token'] as String ?? '',
+              initialCameraPosition: CameraPosition(
+                target: LatLng(14.508, 46.048),
+              ),
+              onMapCreated: (MapboxMapController controller) async {
+                final result = await acquireCurrentLocation();
+                controller.animateCamera(CameraUpdate.newLatLng(result));
+              },
+            );
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Text('Error has occurred: ${snapshot.error.toString()}')
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+    );
   }
 }
